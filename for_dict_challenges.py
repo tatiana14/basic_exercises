0c29e5c39a62359e6fa2e62c1a9ed13bc223606d
Tatiana Smirnova <tatiana.smirnova@iceye.fi>
2022-09-11 16:09:16 +0300

    Add solutions

--- for_dict_challenges.py
+++ for_dict_challenges.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 # Задание 1
 # Дан список учеников, нужно посчитать количество повторений каждого имени ученика
 # Пример вывода:
@@ -5,6 +8,7 @@
 # Маша: 2
 # Петя: 2
 
+print("====== Task1 ======")
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -12,13 +16,16 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Петя'},
 ]
-# ???
-
+repetitions = Counter(student['first_name'] for student in students)
+for name in repetitions:
+    print(f"{name}: {repetitions[name]}")
 
 # Задание 2
 # Дан список учеников, нужно вывести самое часто повторящееся имя
 # Пример вывода:
 # Самое частое имя среди учеников: Маша
+
+print("====== Task2 ======")
 students = [
     {'first_name': 'Вася'},
     {'first_name': 'Петя'},
@@ -26,8 +33,9 @@ students = [
     {'first_name': 'Маша'},
     {'first_name': 'Оля'},
 ]
-# ???
-
+repetitions = Counter(student['first_name'] for student in students)
+most_common_name = max(repetitions, key=repetitions.get)
+print(f'Самое частое имя среди учеников: {most_common_name}')
 
 # Задание 3
 # Есть список учеников в нескольких классах, нужно вывести самое частое имя в каждом классе.
@@ -35,6 +43,7 @@ students = [
 # Самое частое имя в классе 1: Вася
 # Самое частое имя в классе 2: Маша
 
+print("====== Task3 ======")
 school_students = [
     [  # это – первый класс
         {'first_name': 'Вася'},
@@ -45,14 +54,17 @@ school_students = [
         {'first_name': 'Маша'},
         {'first_name': 'Оля'},
     ],[  # это – третий класс
-        {'first_name': 'Женя'},
+        {'first_name': 'Женя'},# ???
+
         {'first_name': 'Петя'},
         {'first_name': 'Женя'},
         {'first_name': 'Саша'},
     ],
 ]
-# ???
-
+for i, students_in_class in enumerate(school_students, start=1):
+    repetitions = Counter(student['first_name'] for student in students_in_class)
+    most_common_name = max(repetitions, key=repetitions.get)
+    print(f"Most common name in class {i}: {most_common_name}")
 
 # Задание 4
 # Для каждого класса нужно вывести количество девочек и мальчиков в нём.
@@ -60,6 +72,7 @@ school_students = [
 # Класс 2a: девочки 2, мальчики 0 
 # Класс 2б: девочки 0, мальчики 2
 
+print("====== Task4 ======")
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '2б', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -72,7 +85,16 @@ is_male = {
     'Миша': True,
     'Даша': False,
 }
-# ???
+for school_class in school:
+    males = 0
+    females = 0
+    for student in school_class['students']:
+        if is_male.get(student['first_name']):
+            males += 1
+        else:
+            females += 1
+    print(f'Class {school_class["class"]}: females: {females}, males {males}')
+
 
 
 # Задание 5
@@ -81,6 +103,7 @@ is_male = {
 # Больше всего мальчиков в классе 3c
 # Больше всего девочек в классе 2a
 
+print("====== Task5 ======")
 school = [
     {'class': '2a', 'students': [{'first_name': 'Маша'}, {'first_name': 'Оля'}]},
     {'class': '3c', 'students': [{'first_name': 'Олег'}, {'first_name': 'Миша'}]},
@@ -91,5 +114,20 @@ is_male = {
     'Олег': True,
     'Миша': True,
 }
-# ???
+
+students_by_gender = []
+for school_class in school:
+    a = [x for x in school_class['students'] if is_male.get(x['first_name'])]
+    b = [x for x in school_class['students'] if not is_male.get(x['first_name'])]
+    students_by_gender.append({'class': school_class['class'], 'males': len(a), 'females': len(b)})
+more_males = max(students_by_gender, key=lambda x: x['males'])
+more_females = max(students_by_gender, key=lambda x: x['females'])
+print('Больше всего мальчиков в классе ', more_males['class'])
+print('Больше всего девочек в классе ', more_females['class'])
+
+
+
+
+
+
 
--- string_challenges.py
+++ string_challenges.py
@@ -1,28 +1,39 @@
 # Вывести последнюю букву в слове
 word = 'Архангельск'
-# ???
-
+print("====== Task1 ======")
+print(word[-1])
 
 # Вывести количество букв "а" в слове
 word = 'Архангельск'
-# ???
-
+print("====== Task2 ======")
+print(word.lower().count('а'))
 
 # Вывести количество гласных букв в слове
 word = 'Архангельск'
-# ???
-
+vowels = set("аоыэуеюия")
+count = 0
+for letter in word.lower():
+    if letter in vowels:
+        count += 1
+print("====== Task3 ======")
+print(count)
 
 # Вывести количество слов в предложении
 sentence = 'Мы приехали в гости'
-# ???
-
+words = sentence.split(' ')
+print("====== Task4 ======")
+print(len(words))
 
 # Вывести первую букву каждого слова на отдельной строке
 sentence = 'Мы приехали в гости'
-# ???
-
+words = sentence.split(' ')
+print("====== Task5 ======")
+for word in words:
+    print(word[0])
 
 # Вывести усреднённую длину слова в предложении
 sentence = 'Мы приехали в гости'
-# ???
+words = sentence.split(' ')
+avg = sum(len(word) for word in words)/len(words)
+print("====== Task6 ======")
+print(avg)
